7343dd20ad87d98a01e3552915e6981f3193856c
Roland Tritsch <roland@tritsch.org>
2014-02-05 12:32:45 +0000

    Hello World

--- src/main.c
+++ src/main.c
@@ -1,20 +1,41 @@
 #include <pebble.h>
 
-Window* my_window;
-TextLayer* text_layer;
+Window* g_main_window;
+TextLayer* g_hello_text_layer;
 
-void handle_init(void) {
-	  my_window = window_create();
-	  text_layer = text_layer_create(GRect(0, 0, 144, 20));
+void window_load(Window* w) {
+  // We will add the creation of the Window's elements here soon!
+  g_hello_text_layer = text_layer_create(GRect(0, 0, 144, 168));
+  text_layer_set_background_color(g_hello_text_layer, GColorClear);
+  text_layer_set_text_color(g_hello_text_layer, GColorBlack);
+  text_layer_set_font(g_hello_text_layer, fonts_get_system_font("RESOURCE_ID_BITHAM_42_BOLD"));
+  text_layer_set_text(text_layer, "Hello Sean!");
+
+  layer_add_child(window_get_root_layer(w), (Layer*)g_hello_text_layer);
 }
 
-void handle_deinit(void) {
-	  text_layer_destroy(text_layer);
-	  window_destroy(my_window);
+void window_unload(Window* w) {
+  // We will safely destroy the Window's elements here!
+  text_layer_destroy(g_hello_text_layer);
+}
+
+void init() {
+  // Initialize the app elements here!
+  g_main_window = window_create();
+  window_set_window_handlers(g_main_window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload,
+  });
+  window_stack_push(g_main_window, true);
+}
+
+void deinit() {
+  // De-initialize elements here to save memory!
+  window_destroy(g_main_window);
 }
 
 int main(void) {
-	  handle_init();
-	  app_event_loop();
-	  handle_deinit();
+  init();
+  app_event_loop();
+  deinit();
 }
